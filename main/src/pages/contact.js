e8c750039e975b737d43c2e90218e04361657834
jbowwman8991 <jbowman8991@gmail.com>
2023-01-30 15:05:01 -0700

    combined information

--- main/src/pages/contact.js
+++ main/src/pages/contact.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./home.css";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact us</h1>
+    <div class="content">
+      <div>Icon</div>
+      <div id="item">
+        <div id="highlight"></div>
+        <span>Orenda-box</span>
+        <div id="text"></div>
+      </div>
+      <br></br>
+      <div class="color"></div>
+      <br></br>
+      <div class="info">Contact us</div>
+      <br></br>
+      <div class="color"></div>
+      <br></br>
       <Link to="/home">Home</Link>
     </div>
   );
--- main/src/pages/home.js
+++ main/src/pages/home.js
@@ -1,11 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./home.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-      <Link to="/contact">Contact us</Link>
+    <div class="content">
+      <div>Icon</div>
+      <div id="item">
+        <div id="highlight"></div>
+        <span>Orenda-box</span>
+        <div id="text"></div>
+      </div>
+      <br></br>
+      <div class="color"></div>
+      <br></br>
+      <div class="info">Info</div>
+      <br></br>
+      <div class="color"></div>
+      <br></br>
+      <Link to="/contact">Contact Us</Link>
     </div>
   );
 };
