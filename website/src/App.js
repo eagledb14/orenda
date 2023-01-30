5d18b6ef303d38f31cdc3b7ed76afc6c421368c9
jbowwman8991 <jbowman8991@gmail.com>
2023-01-30 12:11:03 -0700

    made initial site

--- website/src/App.js
+++ website/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="content">
+      <div>Icon</div>
+      <div id="item">
+        <div id="highlight"></div>
+        <span>Orenda-box</span>
+        <div id="text"></div>
+      </div>
+      <br></br>
+      <div class="color"></div>
+      <br></br>
+      <div class="info">Info</div>
+      <br></br>
+      <div class="color"></div>
+      <br></br>
+      <div class="contact">Contact Us</div>
     </div>
   );
 }
