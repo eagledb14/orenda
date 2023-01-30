3dd7e3c1253e34c173657dfff42a41f247268bb6
jbowwman8991 <jbowman8991@gmail.com>
2023-01-30 14:52:38 -0700

    fixing links

---
+++ main/src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact us</h1>
+      <Link to="/home">Home</Link>
+    </div>
+  );
+};
+
+export default Contact;
